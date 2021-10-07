09aa7b4a60742ece06addeeb9878fc226363b6a4
Michael Maycock <michaelmaycock@gmail.com>
2021-10-07 22:14:38 +0100

    0710-2214

--- einstein/forms.py
+++ einstein/forms.py
@@ -5,12 +5,6 @@ from .models import Language, Topic
 
 class LanguageForm(forms.Form):
     language = forms.ModelChoiceField(
-<<<<<<< HEAD
-        queryset=Language.objects.all().order_by('name'), label="Select a language")
-    topic = forms.ModelChoiceField(
-        queryset=Topic.objects.all().order_by('name'), label="Select a topic")
-=======
         queryset=Language.objects.all().order_by('name'), label="Language")
     topic = forms.ModelChoiceField(
         queryset=Topic.objects.all().order_by('name'), label="Topic")
->>>>>>> 2d134f0 (07102021-1)
--- einstein/urls.py
+++ einstein/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     path('', views.get_code, name="get_code"),
     path('actionUrl', views.importData),
     path('donate', views.donate, name="donate"),
-<<<<<<< HEAD
-    path('contact', views.contact, name="contact")
-=======
     path('contact', views.contact, name="contact"),
     path('about', views.about, name="about")
->>>>>>> 2d134f0 (07102021-1)
 ]
