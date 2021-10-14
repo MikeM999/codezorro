b1d55f966e7c8cb0f844a413b128478c4ca9bc7b
Michael Maycock <michaelmaycock@gmail.com>
2021-10-14 15:27:49 +0100

    1410-6

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'codezorro.herokuapp.com', 'codezorro.com']
 
